73aea87d00f058c954ae9a8551884b32b8e4e22c
scotch_tktk <136587185+sushisk@users.noreply.github.com>
2026-08-10 23:56:04 +0900

    Expose shared JSONL reader

--- src/sts2_training/visualizer/__init__.py
+++ src/sts2_training/visualizer/__init__.py
@@ -1,10 +1,16 @@
-from sts2_training.visualizer.core import EventStore, ReplayLogError, present_event, read_jsonl
-from sts2_training.visualizer.live import LiveRunConfig, LiveRunController
+from sts2_training.visualizer.core import (
+    EventStore,
+    JsonlLogReader,
+    ReplayLogError,
+    present_event,
+    read_jsonl,
+)
+from sts2_training.visualizer.live import LiveRunController
 from sts2_training.visualizer.server import VisualizerApp, make_server
 
 __all__ = [
     "EventStore",
-    "LiveRunConfig",
+    "JsonlLogReader",
     "LiveRunController",
     "ReplayLogError",
     "VisualizerApp",
