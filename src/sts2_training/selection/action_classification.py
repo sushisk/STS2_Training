e53c259bf91c4270cadf221a4a60a5f0e437f5b8
scotch_tktk <136587185+sushisk@users.noreply.github.com>
2026-08-12 12:55:21 +0900

    Classify reward card actions

--- src/sts2_training/selection/action_classification.py
+++ src/sts2_training/selection/action_classification.py
@@ -18,6 +18,7 @@ CARD_ACTION_TYPE = "card"
 CHOICE_CARD_ACTION_TYPE = "choice_card"
 CHOICE_CONFIRM_ACTION_TYPE = "choice_confirm"
 CHOICE_SKIP_ACTION_TYPE = "choice_skip"
+CHOICE_REWARD_CARD_ACTION_TYPE = "choice_reward_card"
 CHOICE_REWARD_POTION_TAKE_ACTION_TYPE = "choice_reward_potion_take"
 CHOICE_REWARD_POTION_REPLACE_ACTION_TYPE = "choice_reward_potion_replace"
 CHOICE_REWARD_SKIP_ACTION_TYPE = "choice_reward_skip"
@@ -52,6 +53,10 @@ def choice_confirm_actions(legal_actions: Sequence[JsonObject]) -> list[JsonObje
 
 def choice_skip_actions(legal_actions: Sequence[JsonObject]) -> list[JsonObject]:
     return _actions_of_type(legal_actions, CHOICE_SKIP_ACTION_TYPE)
+
+
+def reward_card_actions(legal_actions: Sequence[JsonObject]) -> list[JsonObject]:
+    return _actions_of_type(legal_actions, CHOICE_REWARD_CARD_ACTION_TYPE)
 
 
 def reward_potion_take_actions(legal_actions: Sequence[JsonObject]) -> list[JsonObject]:
